a5d38ed06adaf886a5d5b0ba63f333412b3c1921
lokananthan007 <srilogu146@gmail.com>
2024-11-30 10:36:40 +0530

    4th commit

--- src/components/admin/empdetails/Empdetails.js
+++ src/components/admin/empdetails/Empdetails.js
@@ -1,54 +1,61 @@
 import '../empdetails/Empdetails.css';
 
-function Empdetdils(){
-    return(
+function Empdetdils() {
+    const preventScroll = (e) => {
+        e.target.blur(); 
+    };
+
+    return (
         <div className="main-content">
-            <div id='Empdetdils'>
-                <div className='row'>
+            <div id="Empdetdils">
+                <div className="row">
                     <h1>Employee Registration</h1>
                     <form>
-                        <lable>First Name :</lable>
-                        <input type="text"></input>
-                        <lable>Last Name :</lable>
-                        <input type="text"></input>
-                        <lable>Mobile Number :</lable>
-                        <input type="number"></input>
-                        <lable>Alternate Number :</lable>
-                        <input type="number"></input>
-                        <lable>Date Of Birth :</lable>
-                        <input type="date"></input>
-                        <lable>E-mail Id :</lable>
-                        <input type="email"></input>
-                        <lable>Aadhar Number :</lable>
-                        <input type="number"></input>
-                        <lable>Pan Card Number :</lable>
-                        <input type="number"></input>
-                        <lable>Designation: </lable>
-                        <input type='text'></input>
-                        <lable>Emp Id : </lable>
-                        <input type='text'></input>
-                        <lable>Password : </lable>
-                        <input type="password"></input>
+                        <label>First Name:</label>
+                        <input type="text" />
+                        <label>Last Name:</label>
+                        <input type="text" />
+                        <label>Mobile Number:</label>
+                        <input type="number" onWheel={preventScroll} />
+                        <label>Alternate Number:</label>
+                        <input type="number" onWheel={preventScroll} />
+                        <label>Date Of Birth:</label>
+                        <input type="date" />
+                        <label>E-mail Id:</label>
+                        <input type="email" />
+                        <label>Aadhar Number:</label>
+                        <input type="number" onWheel={preventScroll} />
+                        <label>Pan Card Number:</label>
+                        <input type="number" onWheel={preventScroll} />
+                        <label>Date Of Joining:</label>
+                        <input type="date" />
+                        <label>Designation:</label>
+                        <input type="text" />
+                        <label>Emp Id:</label>
+                        <input type="text" />
+                        <label>Password:</label>
+                        <input type="password" />
                         <h1>Address</h1>
-                        <input type="text" placeholder="Line 1"></input>
-                        <input type="text" placeholder="Line 2"></input>
-                        <input type="text" placeholder="City"></input>
-                        <input type="text" placeholder="State"></input>
-                        <input type="number" placeholder="Zip Code"></input>
+                        <input type="text" placeholder="Line 1" />
+                        <input type="text" placeholder="Line 2" />
+                        <input type="text" placeholder="City" />
+                        <input type="text" placeholder="State" />
+                        <input type="number" placeholder="Zip Code" onWheel={preventScroll} />
                         <h1>Bank Details</h1>
-                        <lable>Bank Name : </lable>
-                        <input type="text"></input>
-                        <lable>Branch : </lable>
-                        <input type="text"></input>
-                        <lable>Account Number : </lable>
-                        <input type="number"></input>
-                        <lable>IFSC Code : </lable>
-                        <input type="number"></input>
-                        <button type='submit'>Submit</button>
+                        <label>Bank Name:</label>
+                        <input type="text" />
+                        <label>Branch:</label>
+                        <input type="text" />
+                        <label>Account Number:</label>
+                        <input type="number" onWheel={preventScroll} />
+                        <label>IFSC Code:</label>
+                        <input type="text" />
+                        <button type="submit">Submit</button>
                     </form>
                 </div>
             </div>
         </div>
     );
 }
+
 export default Empdetdils;
